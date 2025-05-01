0744b7e6d73102db2610f91974caac7cdee7a882
Loyal0713 <jtsb0123@gmail.com>
2025-04-30 23:16:34 -0500

    lore fix
    - dragon needs to be killed. technical reason: challenge needs to be completed, lore reason: ethical argument TBD

--- src/main/java/com/cem/mixin/LivingEntityMixin.java
+++ src/main/java/com/cem/mixin/LivingEntityMixin.java
@@ -5,8 +5,11 @@ import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.damagesource.DamageTypes;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.boss.enderdragon.EnderDragon;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
@@ -21,12 +24,15 @@ public abstract class LivingEntityMixin {
 
         // System.out.printf("Thing getting hurt: %s, Damage Source: %s, Attacker: %s, Direct Cause: %s%n", entity, damageSource, attacker, directCause);
 
-        // ignore player checks
         if (entity instanceof ServerPlayer) {
             return;
         }
 
-        // player directly caused damage - ignore
+        if (entity instanceof EnderDragon) {
+            return;
+        }
+
+        // player directly caused damage
         if (attacker instanceof ServerPlayer) {
             cir.setReturnValue(false);
         }
