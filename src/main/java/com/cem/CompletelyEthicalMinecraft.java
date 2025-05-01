1a4c0bb9d6e6049ca7e9ede92e2ec2f0e94c66d8
Loyal0713 <jtsb0123@gmail.com>
2025-04-30 20:18:15 -0500

    initial commit

---
+++ src/main/java/com/cem/CompletelyEthicalMinecraft.java
@@ -0,0 +1,15 @@
+package com.cem;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CompletelyEthicalMinecraft implements ModInitializer {
+	public static final String MOD_ID = "completely-ethical-minecraft";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+	}
+}
---
+++ src/main/java/com/cem/mixin/LivingEntityMixin.java
@@ -0,0 +1,60 @@
+package com.cem.mixin;
+
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public abstract class LivingEntityMixin {
+    @Inject(method = "hurtServer", at = @At("HEAD"), cancellable = true)
+    private void cancelDamage(ServerLevel serverLevel, DamageSource damageSource, float f, CallbackInfoReturnable<Boolean> cir) {
+        LivingEntity entity = (LivingEntity) (Object) this; // convert to LivingEntity
+        Entity attacker = damageSource.getEntity();
+        Entity directCause = damageSource.getDirectEntity();
+
+        // ignore player checks
+        if (entity instanceof ServerPlayer) {
+            return;
+        }
+
+        // player directly caused damage - ignore
+        if (attacker instanceof ServerPlayer) {
+            cir.setReturnValue(false);
+        }
+
+        if (attacker == null && directCause == null) {
+            // various damage types that could be caused "indirectly" by a player
+            // and should be canceled
+            if (damageSource.is(DamageTypes.ON_FIRE) ||
+                    damageSource.is(DamageTypes.IN_FIRE) ||
+                    damageSource.is(DamageTypes.CAMPFIRE) ||
+                    damageSource.is(DamageTypes.LAVA) ||
+                    damageSource.is(DamageTypes.LIGHTNING_BOLT) ||
+                    damageSource.is(DamageTypes.DROWN) ||
+                    damageSource.is(DamageTypes.FALL) ||
+                    damageSource.is(DamageTypes.IN_WALL) ||
+					damageSource.is(DamageTypes.CACTUS) ||
+					damageSource.is(DamageTypes.SWEET_BERRY_BUSH) ||
+					damageSource.is(DamageTypes.STALAGMITE) ||
+                    damageSource.is(DamageTypes.FIREWORKS) ||
+					damageSource.is(DamageTypes.FALLING_BLOCK) ||
+					damageSource.is(DamageTypes.FALLING_ANVIL) ||
+					damageSource.is(DamageTypes.FALLING_STALACTITE) ||
+					damageSource.is(DamageTypes.UNATTRIBUTED_FIREBALL) ||
+					damageSource.is(DamageTypes.EXPLOSION) ||
+					damageSource.is(DamageTypes.PLAYER_EXPLOSION) ||
+					damageSource.is(DamageTypes.SONIC_BOOM) ||
+                    damageSource.is(DamageTypes.FREEZE)
+            ) {
+                cir.setReturnValue(false);
+            }
+        }
+    }
+}
